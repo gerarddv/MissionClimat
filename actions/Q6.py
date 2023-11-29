2cbcb19c99856ae54713975a37b36bba62abeca7
gerarddv <gerard.deviala@gmail.com>
2023-11-29 09:37:00 +0100

    Q6 modifications, test file, add matplot fuynct to print graph

--- actions/Q6.py
+++ actions/Q6.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from utils import display
 from tkinter import ttk
+from utils import db
 
 class Window(tk.Toplevel):
     def __init__(self, parent):
@@ -14,3 +15,43 @@ class Window(tk.Toplevel):
    - par mois, l’évolution de la moyenne des températures minimales
    - par mois, l’évolution des totaux de coûts de travaux tout type confondu""",
                   wraplength=500, anchor="center", font=('Helvetica', '10', 'bold')).grid(sticky="we", row=0)
+
+    def extract_data_temp(self):
+        try:
+            query = "SELECT strftime('%m', date_mesure) as mois, AVG(temperature_min_mesure) FROM Mesures" \
+                    "WHERE code_departement = '38' AND strftime('%Y, dat_mesure) = '2022'" \
+                    "GROUP BY mois" \
+                    "ORDER BY mois;"
+            cursor = db.data.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+        except Exception as e:
+            print("Erreur : " + repr(e))
+        return result
+
+    def extract_data_cout(self):
+        try:
+            query = "SELECT strftime('%m', date_travaux) as mois, SUM(cout_travaux) as total_cout_travaux " \
+                    "FROM Travaux " \
+                    "WHERE strftime('%Y', date_travaux) = '2022' " \
+                    "GROUP BY mois " \
+                    "ORDER BY mois;"
+            cursor = db.data.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+        except Exception as e:
+            print("Erreur : " + repr(e))
+        return result
+    def show_graph_evolution(self):
+        result = self.extract_data_temp()
+        for row in result:
+            print(row)
+
+        result = self.extract_data_cout()
+        for row in result:
+            print(row)
+
+
+    show_graph_evolution()
